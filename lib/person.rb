dbb1b3b12643bb82ac7bab0fb97afbf414fd2465
Valerie Foster <fosterv2@outlook.com>
2019-12-06 00:57:32 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Peson
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(person_job)
+    @job = person_job
+  end
+
+  def breed
+    @job
+  end
+
+end
